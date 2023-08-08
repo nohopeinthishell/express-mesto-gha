b6f5d2fe4e6404a82cbfeb69f61c4f01b5533d04
AAtlantizz <102814825+AAtlantizz@users.noreply.github.com>
2023-08-08 08:47:35 +0200

    sprint 13

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
-app.use('*', (res) => {
+app.use('*', (req, res) => {
   res.status(httpConstants.HTTP_STATUS_NOT_FOUND).send({ message: 'Not Found' });
 });
 
