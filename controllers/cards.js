4cf1b6ae5f5a0ceb78ab48defa70754e248f3c81
AAtlantizz <102814825+AAtlantizz@users.noreply.github.com>
2023-08-20 14:05:53 +0200

    sprint 14 fix

--- controllers/cards.js
+++ controllers/cards.js
@@ -33,7 +33,7 @@ const deleteCard = (req, res, next) => {
       if (card.owner.toString() !== req.user._id) {
         return next(new ForbiddenError('Вы не можете удалить чужую карточку'));
       }
-      return cardSchema.deleteOne(req.params.cardId)
+      return cardSchema.deleteOne()
         .then(() => res.status(httpConstants.HTTP_STATUS_OK).send({ message: 'Карточка удалена' }))
         .catch((err) => next(err));
     })
